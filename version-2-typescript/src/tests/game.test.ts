8659538fbac13c82191a893e86936bf519f88ef2
Stuart Green <stuartngreen@gmail.com>
2019-05-09 14:12:04 +0200

    added grid tests

---
+++ version-2-typescript/src/tests/game.test.ts
@@ -0,0 +1,61 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Game } from '../models/game';
+
+describe('Game Tests', () => {
+
+    let game: Game;
+
+    beforeEach(() => {
+        game = new Game(3, 3);
+
+        game.grid.getCell(0, 0).isAlive = false;
+        game.grid.getCell(0, 1).isAlive = true;
+        game.grid.getCell(0, 2).isAlive = false;
+        game.grid.getCell(1, 0).isAlive = true;
+        game.grid.getCell(1, 1).isAlive = true;
+        game.grid.getCell(1, 2).isAlive = true;
+        game.grid.getCell(2, 0).isAlive = false;
+        game.grid.getCell(2, 1).isAlive = false;
+        game.grid.getCell(2, 2).isAlive = true;
+
+        // Grid start state:
+        //   0 1 2
+        // 0 x o x
+        // 1 o o o
+        // 2 x x o
+    });
+
+    it('should return correct count of live neighbours when cell is alive', () => {
+        let cell = game.grid.getCell(1, 1);
+        let liveCount = game.liveNeighbours(cell);
+        expect(liveCount).to.be.equal(4);
+    });
+
+    it('should return correct count of live neighbours when cell is dead', () => {
+        let cell = game.grid.getCell(0, 2);
+        let liveCount = game.liveNeighbours(cell);
+        expect(liveCount).to.be.equal(3);
+    });
+
+    it('should generate correct state of grid on iteration as per game rules', () => {
+        game.nextIteration();
+        
+        // Expected grid state:
+        //   0 1 2
+        // 0 o o o
+        // 1 o x o
+        // 2 x x o
+
+        expect(game.grid.getCell(0, 0).isAlive).to.be.true;
+        expect(game.grid.getCell(0, 1).isAlive).to.be.true;
+        expect(game.grid.getCell(0, 2).isAlive).to.be.true;
+        expect(game.grid.getCell(1, 0).isAlive).to.be.true;
+        expect(game.grid.getCell(1, 1).isAlive).to.be.false;
+        expect(game.grid.getCell(1, 0).isAlive).to.be.true;
+        expect(game.grid.getCell(2, 0).isAlive).to.be.false;
+        expect(game.grid.getCell(2, 1).isAlive).to.be.false;
+        expect(game.grid.getCell(2, 2).isAlive).to.be.true;
+    });
+
+});
--- version-2-typescript/src/tests/grid.test.ts
+++ version-2-typescript/src/tests/grid.test.ts
@@ -1,24 +1,24 @@
-import { expect } from "chai";
-import "mocha";
-import { Grid } from "../models/grid";
+import 'mocha';
+import { expect } from 'chai';
+import { Grid } from '../models/grid';
 
-describe("Grid Tests", () => {
+describe('Grid Tests', () => {
 
     let grid: Grid;
 
     beforeEach(() => {
-        grid = new Grid(10,10);
+        grid = new Grid(10, 10);
     });
 
-    it("should return cell with valid parameters", () => {
-        let cell = grid.getCell(4,5);
+    it('should return cell with valid parameters', () => {
+        let cell = grid.getCell(4, 5);
         expect(cell).to.be.not.undefined;
         expect(cell.r).to.be.equal(4);
         expect(cell.c).to.be.equal(5);
     });
 
-    it("should return null with invalid parameters", () => {
-        let cell = grid.getCell(-1,-1);
+    it('should return null with invalid parameters', () => {
+        let cell = grid.getCell(-1, -1);
         expect(cell).to.be.null;
     });
 
